d5a4bcb29d08802cb588f005c65f9acb8edfb7d4
Michaelgp96 <michaelwow3@gmail.com>
2026-05-02 04:47:23 +0000

    feat: embed Unity WebGL desde itch.io

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -14,7 +14,7 @@ export default function Game({ user }) {
         <div className="w-full max-w-5xl">
           <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
             <iframe
-              src="https://itch.io/embed-upload/4534010?color=1a3a2a"
+              src="https://itch.io/embed-upload/17385218?color=1a3a2a"
               className="absolute top-0 left-0 w-full h-full rounded-2xl border-2 border-green-700"
               allowFullScreen
               allow="autoplay; fullscreen *; pointer-lock *; encrypted-media;"
